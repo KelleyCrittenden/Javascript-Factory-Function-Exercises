255a3a39e9930a09a105c64217fecb1eaa5e8fb9
Kelley Crittenden <kelleylcrittenden@gmail.com>
2020-06-17 23:12:28 -0500

    finsihed lightning exercises

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,8 @@
 import createDoctor from "./doctorFactory.js"
-import createPet from "./petFactory.js"
+import bowWowKennels from "./petFactory.js"
 
 const drReed = createDoctor("Jim Reed", "Podiatrist", "113 Martin Street")
 console.log(drReed)
 
-const bishop = createPet("Bishop", "Miniature Schnauzer")
-const remy = createPet("Remy", "Giant Schnauzer")
 
-console.log(bishop)
-console.log(remy)
+console.log(bowWowKennels)
